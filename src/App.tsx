7e8c9b33b4c589a139a877a3eace705b2507b8b8
Oleg Somov <gog0125@mail.ru>
2024-09-15 15:15:41 +0300

    create main and profile pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Main, Profile } from 'pages';
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from 'widgets';
+
+const Loading = () => <h2>🌀 Loading...</h2>;
 
 export const App = () => (
   <div className='app'>
+    <Header userName='ivan1234' />
+    <main className='main'>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/profile/:id' element={<Profile />} />
+        </Routes>
+      </Suspense>
+    </main>
   </div>
 );
