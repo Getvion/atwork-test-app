089372fc1c56273916a28411372d981e6235d490
Oleg Somov <gog0125@mail.ru>
2024-09-15 14:18:44 +0300

    create UserCard widget

---
+++ src/widgets/UserCard/UserCard.tsx
@@ -0,0 +1,30 @@
+import DotsIcon from 'assets/icons/dots.svg?react';
+import AvatarImage from 'assets/images/avatar.png';
+import { IUserData } from 'shared/types';
+
+import styles from './UserCard.module.scss';
+
+interface IUserCard extends React.ComponentPropsWithoutRef<'div'> {
+  userData: IUserData;
+}
+
+export const UserCard = ({ userData }: IUserCard) => {
+  const onDotsClick = () => {};
+
+  return (
+    <div className={styles.card}>
+      <img src={AvatarImage} alt='avatar' className={styles.card__image} />
+      <div className={styles.card__info}>
+        <div className={styles.info__container}>
+          <span className={styles.card__username}>{userData.name}</span>
+          <span className={styles.card__company}> {userData.company.name}</span>
+        </div>
+        <span className={styles.card__city}> {userData.address.city}</span>
+
+        <button onClick={onDotsClick} aria-label='dots-icon' className={styles.dots}>
+          <DotsIcon />
+        </button>
+      </div>
+    </div>
+  );
+};
