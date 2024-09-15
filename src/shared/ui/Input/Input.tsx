edc93d75d86d8f031b2484701538d1f76bf2b9c7
Oleg Somov <gog0125@mail.ru>
2024-09-15 21:41:11 +0300

    refactor input component

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,5 +1,4 @@
 import CloseIcon from 'assets/icons/close.svg?react';
-import { useState } from 'react';
 
 import styles from './Input.module.scss';
 
@@ -8,40 +7,20 @@ interface IInputProps extends Omit<React.ComponentPropsWithoutRef<'input'>, 'onC
   textContent: string;
 }
 
-export const Input = ({ onChange, onBlur, onFocus, onClearClick, textContent }: IInputProps) => {
-  const [inputValue, setInputValue] = useState('');
-
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-
-    if (onChange) {
-      onChange(e);
-    }
-  };
-
-  const onCloseClick = () => {
-    setInputValue('');
-
-    onClearClick();
-  };
-
-  return (
-    <label htmlFor={textContent} className={styles.label}>
-      <span className={styles.text}>{textContent}</span>
-      <input
-        className={styles.input}
-        id={textContent}
-        type='text'
-        value={inputValue}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        onChange={onInputChange}
-      />
-      {inputValue && (
-        <button aria-label='close' className={styles.close__button} onClick={onCloseClick}>
-          <CloseIcon />
-        </button>
-      )}
-    </label>
-  );
-};
+export const Input = ({ onChange, onClearClick, textContent, value }: IInputProps) => (
+  <label htmlFor={textContent} className={styles.label}>
+    <span className={styles.text}>{textContent}</span>
+    <input
+      className={styles.input}
+      id={textContent}
+      type='text'
+      value={value}
+      onChange={onChange}
+    />
+    {value && (
+      <button aria-label='close' className={styles.close__button} onClick={onClearClick}>
+        <CloseIcon />
+      </button>
+    )}
+  </label>
+);
